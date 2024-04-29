56088e89db54526c819ccd1fdf4e8eb987b9022f
HManiac74 <hmaniac28@yahoo.de>
2024-04-29 20:29:01 +0200

    Base Character Class

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright 2024 Rubysoft
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,19 @@
+// Copyright 2024 Rubysoft
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
